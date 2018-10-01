b1af121ee75215e8e67cc962684ee33f39c1e7d1
James Grimmelmann <james.grimmelmann@cornell.edu>
2018-10-01 11:14:32 -0400

    Peewee fixes

--- class9/peewee-sqlite.py
+++ class9/peewee-sqlite.py
@@ -2,15 +2,15 @@ from peewee import *
 
 db = SqliteDatabase('menu.sqlite')
 
-class items(Model):
+class Items(Model):
     Name = CharField()
     Price = CharField()
     Type = CharField()
     class Meta:
         database = db
 
-for i in items.select():
+for i in Items.select():
     print (i.Name + ': ' + i.Price)
 
-mainsList = items.select().where(items.Type == 'Mains')
+mainsList = Items.select().where(Items.Type == 'Mains')
 print ('There are ' + str(len(mainsList)) + ' main dishes.')
